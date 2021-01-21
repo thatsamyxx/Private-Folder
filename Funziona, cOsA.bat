664c82db8aba53834368e13c113932270912bcd8
Amanda Maretti <65337126+thatsamyxx@users.noreply.github.com>
2021-01-21 17:10:44 +0100

    Update Funziona, cOsA.bat

--- Funziona, cOsA.bat
+++ Funziona, cOsA.bat
@@ -1,16 +1,23 @@
 @ECHO OFF
+echo Made by Thatsamyxx on GitHub
 goto START
 :START
 if EXIST "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}" goto UNLOCK
-if NOT EXIST "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}" goto FAILED
+if NOT EXIST Private goto CREATEFOLDER
+if EXIST Private goto HIDE
 :UNLOCK
-echo Enter password to Unlock Your Secure Folder
+echo Enter password to unlock your folder
 set/p "pass=>"
-if NOT %pass%== 1415926535897932384626433832 goto FAIL
+rem ////////////////////////////////////////////////////////////////////////////////////////
+rem ///////////////////////////  SET YOUR PASSWORD DOWN HERE!  /////////////////////////////
+rem ////////////////////////////////////////////////////////////////////////////////////////
+rem ////////  REPLACE *HERE* WITH THE PASSWORD YOU WANT TO USE TO UNLOCK THE FOLDER  ///////
+rem ////////////////////////////////////////////////////////////////////////////////////////
+if NOT %pass%== *HERE* goto FAIL
 attrib -h -s "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
 ren "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}" Private
 echo Folder Unlocked successfully, you have now access to the file for 15 seconds
-timeout /t 15 >nul
+ping 192.0.2.2 -n 1 -w 15000 > nul
 goto LOCK
 :FAIL
 echo Invalid password
@@ -19,10 +26,20 @@ goto UNLOCK
 ren Private "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
 attrib +h +s "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
 echo Session expired
-timeout /t 1 >nul
-goto End
-:FAILED
-echo Fatal error, run config.bat twice first!
 timeout /t 3 >nul
-goto end
-:End
+goto END
+:CREATEFOLDER
+md Private
+echo I've just created a new folder named 'Private', now put all your sEcReT stuff in there, then open this file again
+timeout /t 7 >nul
+:HIDE
+echo Hiding folder...
+ren Private "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
+ping 192.0.2.2 -n 1 -w 1000 > nul
+attrib +h +s "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
+ping 192.0.2.2 -n 1 -w 4000 > nul
+echo Folder hidden! Now set a password by right-clicking this file, and choosing 'Edit'
+ping 192.0.2.2 -n 1 -w 6000 > nul
+echo Don't worry, you will not mess anything up.
+timeout /t 7 >nul
+:END
